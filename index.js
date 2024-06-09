bd3d3b0b570724deb3de0f5a1b9db8622e5bc026
chandreshbodra-55 <bodrachandresh@gmail.com>
2024-06-09 13:43:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,122 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-require("./db/config");
-const User = require('./db/User');
-const Product = require("./db/Product")
-const Jwt = require('jsonwebtoken');
-const jwtKey = 'e-com';
-const app = express();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-app.use(express.json());
-app.use(cors());
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.post("/register", async (req, resp) => {
-    let user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password
-    Jwt.sign({result}, jwtKey, {expiresIn:"2h"},(err,token)=>{
-        if(err){
-            resp.send("Something went wrong")  
-        }
-        resp.send({result,auth:token})
-    })
-})
-
-app.post("/login", async (req, res) => {
-  if (req.body.password && req.body.email) {
-      let user = await User.findOne({ email: req.body.email, password: req.body.password }).select("-password");
-      if (user) {
-          Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
-              if (err) {
-                  return res.status(500).send("Something went wrong");
-              }
-              res.send({ user, auth: token });
-          });
-      } else {
-          res.send({ result: "No User found" });
-      }
-  } else {
-      res.send({ result: "No User found" });
-  }
-});
-
-
-app.post("/add-product", verifyToken, async (req, resp) => {
-    let product = new Product(req.body);
-    let result = await product.save();
-    resp.send(result);
-});
-
-app.get("/products", verifyToken, async (req, resp) => {
-    const products = await Product.find();
-    if (products.length > 0) {
-        resp.send(products)
-    } else {
-        resp.send({ result: "No Product found" })
-    }
-});
-
-app.delete("/product/:id", verifyToken, async (req, resp) => {
-    let result = await Product.deleteOne({ _id: req.params.id });
-    resp.send(result)
-}),
-
-    app.get("/product/:id", verifyToken, async (req, resp) => {
-        let result = await Product.findOne({ _id: req.params.id })
-        if (result) {
-            resp.send(result)
-        } else {
-            resp.send({ "result": "No Record Found." })
-        }
-    })
-
-app.put("/product/:id", verifyToken, async (req, resp) => {
-    let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
-});
-
-app.put("/product/:id", verifyToken, async (req, resp) => {
-    let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
-});
-
-app.get("/search/:key",verifyToken, async (req, resp) => {
-    let result = await Product.find({
-        "$or": [
-            {
-                name: { $regex: req.params.key }  
-            },
-            {
-                company: { $regex: req.params.key }
-            },
-            {
-                category: { $regex: req.params.key }
-            }
-        ]
-    });
-    resp.send(result);
-});
-
-function verifyToken(req,res,next){
-  let token =req.headers['authorization'];
-  if(token){
-    token = token.split(' ')[1];
-    Jwt.verify(token, jwtKey, (err, valid)=>{
-      if(err){
-        res.status(401).send({result:"Please provide valid token "})
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(403).send({result: "Please add token with header"}) 
-  }
-};
-app.listen(4000);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
